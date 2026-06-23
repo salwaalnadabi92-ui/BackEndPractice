411f39d199e910811de20ea4d677311e4595552f
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-06-23 09:41:15 +0400

    implement properties for class flight

--- FlightManagementSystem/Models/Flight.cs
+++ FlightManagementSystem/Models/Flight.cs
@@ -6,5 +6,28 @@ namespace FlightManagementSystem.Models
 {
     public class Flight
     {
+
+        public int flightId { get; set; }  
+        public string flightCode { get; set; }
+        public int aircraftId { get; set; }
+        public int pilotId { get; set; }
+        public string origin { get; set; }
+        public string destination { get; set; }
+        public string departureDate { get; set; }
+
+        public string departureTime { get; set; }
+        public decimal ticketPrice { get; set; }
+        public int vailableSeats { get; set; }
+        public string status { get; set; }
+
+
+
+
+
+
+
+
+
+
     }
 }
