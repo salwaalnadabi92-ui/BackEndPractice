d2453996cc15bf23dc7136c2c766bf20a355d08a
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-06-23 09:46:53 +0400

    add  properties for class booking

--- FlightManagementSystem/Models/Booking.cs
+++ FlightManagementSystem/Models/Booking.cs
@@ -6,5 +6,26 @@ namespace FlightManagementSystem.Models
 {
     public class Booking
     {
+
+        public int bookingId { get; set; }  
+        public int passengerId { get; set; }
+        public int flightId { get; set; }
+        public string seatNumber { get; set; }
+        public string bookingDate { get; set; }
+        public decimal totalPrice { get; set; }
+        public string status { get; set; }
+        
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
