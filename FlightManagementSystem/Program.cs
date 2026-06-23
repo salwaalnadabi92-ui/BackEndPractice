fcd277e06ab27cd9d300410e4c79fb270c96e79f
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-06-23 10:00:13 +0400

    add main menu

--- FlightManagementSystem/Program.cs
+++ FlightManagementSystem/Program.cs
@@ -4,7 +4,139 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+
+
+            bool exit = false;
+
+            while (exit == false)
+            {
+
+                Console.WriteLine(" =======================================");
+                Console.WriteLine("FLIGHT MANAGEMENT SYSTEM ");
+                Console.WriteLine(" =======================================");
+                Console.WriteLine("1.Register a Passenger ");
+                Console.WriteLine("2. Add an Aircraft ");
+                Console.WriteLine("3.Register a Pilot");
+                Console.WriteLine("4.View All Flights ");
+                Console.WriteLine("5. Schedule a Flight ");
+                Console.WriteLine("6.Book a Flight");
+                Console.WriteLine("7.Cancel a Booking ");
+                Console.WriteLine("8.Depart a Flight");
+                Console.WriteLine("9.Cancel a Flight");
+                Console.WriteLine("10.Passenger Booking History");
+                Console.WriteLine("11. Flight Revenue & Load Factor Report");
+                Console.WriteLine("0.Exit");
+                Console.WriteLine("========================================");
+                Console.WriteLine("  Enter your choose");
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+
+
+
+                }//switch
+            }//while
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                }
     }
 }
