3d0e280c53ddc903154faface046bca1d6495f14
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-06-30 14:13:39 +0400

    save source tag for enrollment

--- UniversitySystemERD&Models/models/Enrollment.cs
+++ UniversitySystemERD&Models/models/Enrollment.cs
@@ -7,12 +7,12 @@ namespace UniversitySystemERD_Models.models
     public class Enrollment
 
     {
-       public int enrollmentId {  get; set; }
-       public int    studentId { get; set; }
-       public int courseId { get; set; }
-       public DateTime  enrollmentDate { get; set; }
-       public string    finalGrade { get; set; }
-       public string  status { get; set; }
+       public int enrollmentId {  get; set; }//system genrtated
+       public int    studentId { get; set; }//from list 
+       public int courseId { get; set; }//from list 
+       public DateTime  enrollmentDate { get; set; }//system genrated
+       public string    finalGrade { get; set; }//system calculated
+       public string  status { get; set; }//user input 
 
 
     }
