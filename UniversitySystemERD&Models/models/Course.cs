95db0c84c250d44da5ddbe36360f23ca06ce4e13
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-06-30 15:26:37 +0400

    implement Constraints in class course

--- UniversitySystemERD&Models/models/Course.cs
+++ UniversitySystemERD&Models/models/Course.cs
@@ -1,18 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace UniversitySystemERD_Models.models
 {
     public class Course
     {
-       
+
+        [Key]
+        [Required]
         public int courseId {  get; set; }//user input 
+
+        [Required]
+        [MaxLength(10)]
         public string courseCode { get; set; }//user input 
+        [Required]
+        [MaxLength(150)]
         public string courseTitle { get; set; }//user input 
+        [Required]
+        [Range(1, 6)]
         public int creditHours { get; set; }//system calculated 
+        [ForeignKey("department")]
         public int departmentId { get; set; }//from the list 
+        [ForeignKey("instrutor")]
         public int instructorId { get; set; }//from the list
+
+        [Required]
+        [MaxLength(20)]
         public string semesterOffered { get; set; }//system calculated
 
 
