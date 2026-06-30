ea5f715e815a485fcbfbc08ca048e31757da877a
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-06-30 11:46:36 +0400

    add  attribute student class

--- UniversitySystemERD&Models/models/Student.cs
+++ UniversitySystemERD&Models/models/Student.cs
@@ -8,13 +8,13 @@ namespace UniversitySystemERD_Models.models
     public class Student
     {
 
-       public int studentId { get; set; }
-       public string fullName { get; set; }
-        public string email { get; set; }
-        public string phoneNumber { get; set; }
-        public DateTime dateOfBirth { get; set; }
-        public int enrollmentYear { get; set; }
-        public decimal gpa { get; set; }
+       public int studentId { get; set; }//system gentarted
+       public string fullName { get; set; }//user input 
+        public string email { get; set; }//system gentrated
+        public string phoneNumber { get; set; }//user input 
+        public DateTime dateOfBirth { get; set; }//user input 
+        public int enrollmentYear { get; set; }//user input 
+        public decimal gpa { get; set; }//system calcluated
 
 
 
