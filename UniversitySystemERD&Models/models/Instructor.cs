c363f740eb1ba8869f0bbb2343214c1b31d33506
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-06-30 14:46:53 +0400

    implement   Constraints  in class  Instructor

--- UniversitySystemERD&Models/models/Instructor.cs
+++ UniversitySystemERD&Models/models/Instructor.cs
@@ -1,17 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace UniversitySystemERD_Models.models
 {
     public class Instructor
     {
-       public int  instructorId {  get; set; }//system genrated 
-       public  string fullName { get; set; }//user input 
+
+
+
+        [Key]
+        [Required]
+        public int  instructorId {  get; set; }//system genrated 
+        [Required]
+        [MaxLength(100)]
+        public string fullName { get; set; }//user input 
+        [Required]
+        [MaxLength(150)]
         public string email { get; set; }//system genrated
+        [MaxLength(20)]
         public string  officeNumber { get; set; }//user input 
+        [Required]
         public DateTime hireDate { get; set; }//system gentrated
+        [Required]
         public decimal salary { get; set; }//user input 
+        [Required]
+        [MaxLength(50)]
         public  string academicTitle { get; set; }//user input 
 
 
