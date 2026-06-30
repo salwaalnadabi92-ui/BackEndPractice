7e832d7904417c1423b220487bd52cac31b179e1
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-06-30 15:38:57 +0400

    implement unique Constraint in some class

--- UniversitySystemERD&Models/models/Course.cs
+++ UniversitySystemERD&Models/models/Course.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -6,6 +7,8 @@ using System.Text;
 
 namespace UniversitySystemERD_Models.models
 {
+    [Index(nameof(courseCode), IsUnique = true)]
+
     public class Course
     {
 
--- UniversitySystemERD&Models/models/Enrollment.cs
+++ UniversitySystemERD&Models/models/Enrollment.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace UniversitySystemERD_Models.models
@@ -7,12 +9,22 @@ namespace UniversitySystemERD_Models.models
     public class Enrollment
 
     {
-       public int enrollmentId {  get; set; }//system genrtated
-       public int    studentId { get; set; }//from list 
-       public int courseId { get; set; }//from list 
-       public DateTime  enrollmentDate { get; set; }//system genrated
-       public string    finalGrade { get; set; }//system calculated
-       public string  status { get; set; }//user input 
+
+
+        [Key]
+        [Required]
+        public int enrollmentId {  get; set; }//system genrtated
+        [ForeignKey("student")]
+        public int    studentId { get; set; }//from list 
+        [ForeignKey("course")]
+        public int courseId { get; set; }//from list 
+        [Required]
+        public DateTime  enrollmentDate { get; set; }//system genrated
+        [MaxLength(2)]
+        public string    finalGrade { get; set; }//system calculated
+        [Required]
+        [MaxLength(20)]
+        public string  status { get; set; }//user input 
 
 
     }
