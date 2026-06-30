076d68f5622ae361f42aa6dac6982133ed4a6afc
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-06-30 12:16:32 +0400

    add source tag in class course

--- UniversitySystemERD&Models/models/Course.cs
+++ UniversitySystemERD&Models/models/Course.cs
@@ -7,13 +7,13 @@ namespace UniversitySystemERD_Models.models
     public class Course
     {
        
-        public int courseId {  get; set; }
-        public string courseCode { get; set; }
-        public string courseTitle { get; set; }
-        public int creditHours { get; set; }
-        public int departmentId { get; set; }
-        public int instructorId { get; set; }
-        public string semesterOffered { get; set; }
+        public int courseId {  get; set; }//user input 
+        public string courseCode { get; set; }//user input 
+        public string courseTitle { get; set; }//user input 
+        public int creditHours { get; set; }//system calculated 
+        public int departmentId { get; set; }//from the list 
+        public int instructorId { get; set; }//from the list
+        public string semesterOffered { get; set; }//system calculated
 
 
 
