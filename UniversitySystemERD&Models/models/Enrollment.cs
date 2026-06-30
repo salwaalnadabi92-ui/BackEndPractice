2bd7912a47379f9f534181f4d26ff79837d711b9
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-06-30 12:23:58 +0400

    add Attribute in class enrollment

--- UniversitySystemERD&Models/models/Enrollment.cs
+++ UniversitySystemERD&Models/models/Enrollment.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace UniversitySystemERD_Models.models
 {
-    internal class Enrollment
+    public class Enrollment
+
     {
+       public int enrollmentId {  get; set; }
+       public int    studentId { get; set; }
+       public int courseId { get; set; }
+       public DateTime  enrollmentDate { get; set; }
+       public string    finalGrade { get; set; }
+       public string  status { get; set; }
+
+
     }
 }
