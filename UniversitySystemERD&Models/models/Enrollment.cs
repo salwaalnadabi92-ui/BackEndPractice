5c4062c166f25fd656ba24ff6c79d0c13ace6876
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-06-30 11:40:17 +0400

    implement modles and classes

---
+++ UniversitySystemERD&Models/models/Enrollment.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniversitySystemERD_Models.models
+{
+    internal class Enrollment
+    {
+    }
+}
---
+++ UniversitySystemERD&Models/models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace UniversitySystemERD_Models.models
+{
+    public class Student
+    {
+
+       public int studentId { get; set; }
+       public string fullName { get; set; }
+        public string email { get; set; }
+        public string phoneNumber { get; set; }
+        public DateTime dateOfBirth { get; set; }
+        public int enrollmentYear { get; set; }
+        public decimal gpa { get; set; }
+
+
+
+
+
+    }
+}
