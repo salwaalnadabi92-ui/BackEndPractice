557eaa4228f3aefb863480fa467361ea6b5f4daf
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-06-30 11:56:26 +0400

    add Attribute in class instructor

--- UniversitySystemERD&Models/models/Instructor.cs
+++ UniversitySystemERD&Models/models/Instructor.cs
@@ -4,7 +4,23 @@ using System.Text;
 
 namespace UniversitySystemERD_Models.models
 {
-    internal class Instructor
+    public class Instructor
     {
+       public int  instructorId {  get; set; }//system genrated 
+       public  string fullName { get; set; }//user input 
+        public string email { get; set; }//system genrated
+        public string  officeNumber { get; set; }//user input 
+        public DateTime hireDate { get; set; }//system gentrated
+        public decimal salary { get; set; }//user input 
+        public  string academicTitle { get; set; }//user input 
+
+
+
+
+
+
+
+
+
     }
 }
