b5c4ce6e5eb1c325367908bd651ffcce8b346fb7
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-06-30 12:13:41 +0400

    add ttributes in class course

--- UniversitySystemERD&Models/models/Course.cs
+++ UniversitySystemERD&Models/models/Course.cs
@@ -4,7 +4,22 @@ using System.Text;
 
 namespace UniversitySystemERD_Models.models
 {
-    internal class Course
+    public class Course
     {
+       
+        public int courseId {  get; set; }
+        public string courseCode { get; set; }
+        public string courseTitle { get; set; }
+        public int creditHours { get; set; }
+        public int departmentId { get; set; }
+        public int instructorId { get; set; }
+        public string semesterOffered { get; set; }
+
+
+
+
+
+
+
     }
 }
--- UniversitySystemERD&Models/models/Department.cs
+++ UniversitySystemERD&Models/models/Department.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace UniversitySystemERD_Models.models
 {
-    internal class Department
+    public class Department
     {
+        
+      public int departmentId {  get; set; }// system genrtated
+      public string departmentName {  get; set; }//user input 
+      public string building { get; set; }//user input 
+      public decimal budget { get; set; }//user input 
+      public int headInstructorId { get; set; }//user input 
+
+
+
+
     }
 }
