4e827a0c115bb4542cd1de738f2f742fbde3c7c0
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-07-01 16:01:27 +0400

    implement Attribute   in class product

--- E-CommerceSystemERD&Models/modles/Product.cs
+++ E-CommerceSystemERD&Models/modles/Product.cs
@@ -4,7 +4,28 @@ using System.Text;
 
 namespace E_CommerceSystemERD_Models.modles
 {
-    internal class Product
+    public class Product
     {
+
+        
+           public  int  productId { get; set; }//int Primary Key, auto-generated, not null
+           public string  productName { get; set; }//string Required, max length 150
+           public string  description { get; set; }//string Optional, max length 1000
+           public decimal   price { get; set; }// decimal Required, must be greater than 0
+           public int  stockQuantity { get; set; }//int Required, must be greater than or equal to 0, default 0
+           public string   imageUrl { get; set; }//string Optional, max length 300
+           public  int  categoryId { get; set; }//int Foreign Key to Category, not null
+           public DateTime createdAt { get; set; }//DateTime Required
+           public  bool isAvailable { get; set; } //bool Default true
+
+
+
+
+
+
+
+
+
+
     }
 }
