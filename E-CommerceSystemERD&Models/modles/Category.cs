fb67e6839b77a156152d685c1fd5c205bcdc3d7a
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-07-01 15:40:55 +0400

    implement Attribute in calss category

--- E-CommerceSystemERD&Models/modles/Category.cs
+++ E-CommerceSystemERD&Models/modles/Category.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace E_CommerceSystemERD_Models.modles
 {
-    internal class Category
+    public  class Category
     {
+
+                public int  categoryId { get; set; }//int Primary Key, auto-generated, not null
+                public string categoryName { get; set; }//string Required, unique, max length 100
+                public string description { get; set; }//string Optional, max length 500
+                public string imageUrl { get; set; }//string Optional, max length 300
+
+
+
+
+
     }
 }
