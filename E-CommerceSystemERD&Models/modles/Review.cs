71028d0cce8a5db0c767535f258de9637c27f0f2
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-07-01 16:14:25 +0400

    implement Attribute in class review

--- E-CommerceSystemERD&Models/modles/Review.cs
+++ E-CommerceSystemERD&Models/modles/Review.cs
@@ -4,7 +4,19 @@ using System.Text;
 
 namespace E_CommerceSystemERD_Models.modles
 {
-    internal class Review
+    public class Review
     {
+
+
+      public int reviewId {  get; set; }
+      public int userId {  get; set; }
+
+       public int  productId {  get; set; }
+
+        public   int  rating {  get; set; }
+
+       public   string comment {  get; set; }
+        public   DateTime  reviewDate {  get; set; }
+
     }
 }
