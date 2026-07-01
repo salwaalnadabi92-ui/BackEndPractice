f2b8e474b86e067804bf6d137cffdc67f7d18ad3
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-07-01 16:06:46 +0400

    implement Attribute in calss order

--- E-CommerceSystemERD&Models/modles/Order.cs
+++ E-CommerceSystemERD&Models/modles/Order.cs
@@ -4,7 +4,20 @@ using System.Text;
 
 namespace E_CommerceSystemERD_Models.modles
 {
-    internal class Order
+    public  class Order
     {
+
+        public int orderId { get; set; }// int Primary Key, auto-generated, not null
+        public int  userId { get; set; }//int Foreign Key to User, not null
+        public DateTime orderDate { get; set; } //DateTime Required
+        public decimal totalAmount { get; set; } //decimal Required, must be greater than or equal to 0
+        public string   status { get; set; }//string Required, max length 30, default "Pending"
+        public string  shippingAddress { get; set; }//string Required, max length 300
+         public string  paymentMethod { get; set; }//string Required, max length 50
+
+
+
+
+
     }
 }
