b07a6f1300936f39d637da80d1fa4974afa6194c
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-07-01 08:49:45 +0400

    change source tag for booking status in booking service

--- FlightManagementSystem/Models/Booking.cs
+++ FlightManagementSystem/Models/Booking.cs
@@ -13,7 +13,7 @@ namespace FlightManagementSystem.Models
         public string seatNumber { get; set; }//system genrated 
         public string bookingDate { get; set; }//system genrated
         public decimal totalPrice { get; set; }// from flight.ticket price
-        public string  bookingStatus { get; set; }//user input
+        public string  bookingStatus { get; set; }//defualt value
         
 
                                                                                                                                                                                                                               
