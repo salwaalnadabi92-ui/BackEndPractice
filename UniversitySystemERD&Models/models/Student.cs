c4c8d863074783d07a3b5cc03be6c1069338a93f
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-07-01 11:36:42 +0400

    apply Navigation for student class

--- UniversitySystemERD&Models/models/Student.cs
+++ UniversitySystemERD&Models/models/Student.cs
@@ -41,6 +41,9 @@ namespace UniversitySystemERD_Models.models
         public decimal gpa { get; set; } = 0.0m;//system calcluated
 
 
+        public List<Enrollment> enrollment{ get; set; } // Navigation property 
+
+
 
 
 
