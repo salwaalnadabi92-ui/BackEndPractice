84a44c0197fd0656b3018eaa7252c053af021a39
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-07-05 08:54:25 +0400

    save constrain in class order

--- E-CommerceSystemERD&Models/modles/Order.cs
+++ E-CommerceSystemERD&Models/modles/Order.cs
@@ -1,19 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace E_CommerceSystemERD_Models.modles
 {
     public  class Order
     {
-
-        public int orderId { get; set; }// int Primary Key, auto-generated, not null
-        public int  userId { get; set; }//int Foreign Key to User, not null
-        public DateTime orderDate { get; set; } //DateTime Required
-        public decimal totalAmount { get; set; } //decimal Required, must be greater than or equal to 0
-        public string   status { get; set; }//string Required, max length 30, default "Pending"
-        public string  shippingAddress { get; set; }//string Required, max length 300
-         public string  paymentMethod { get; set; }//string Required, max length 50
+        [key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int orderId { get; set; }
+        [Required]
+        [ForeignKey("")]
+        public int  userId { get; set; }
+        [Required]
+        public DateTime orderDate { get; set; }
+        [Required]
+        [Range(0,double.MaxValue)]
+        public decimal totalAmount { get; set; } // must be greater than or equal to 0
+        [Required]
+        [MaxLength(30)]
+        public string   status { get; set; }= "Pending" 
+        [Required]
+        [MaxLength(300)]
+        public string  shippingAddress { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string  paymentMethod { get; set; }
 
 
 
