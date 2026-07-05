3524356c03e4c01010a9fbbfcb6ddc10aab09b92
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-07-05 10:50:18 +0400

    save constrain in review class

--- E-CommerceSystemERD&Models/modles/Review.cs
+++ E-CommerceSystemERD&Models/modles/Review.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace E_CommerceSystemERD_Models.modles
@@ -7,15 +9,24 @@ namespace E_CommerceSystemERD_Models.modles
     public class Review
     {
 
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int reviewId {  get; set; }
 
-      public int reviewId {  get; set; }
-      public int userId {  get; set; }
+        [Required]
+        [ForeignKey("")]
+        public int userId {  get; set; }
 
-       public int  productId {  get; set; }
-
+        [Required]
+        [ForeignKey("")]
+        public int  productId {  get; set; }
+        [Required]
+        [Range(1, 5)]
         public   int  rating {  get; set; }
-
-       public   string comment {  get; set; }
+        [MaxLength(1000)]
+        public   string ? comment {  get; set; }
+        [Required]
         public   DateTime  reviewDate {  get; set; }
 
     }
