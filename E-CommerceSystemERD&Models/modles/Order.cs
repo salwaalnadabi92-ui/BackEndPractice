996d3507a1633b090d17d1fc690d94c9e1596d55
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-07-05 14:47:20 +0400

    save

--- E-CommerceSystemERD&Models/modles/Order.cs
+++ E-CommerceSystemERD&Models/modles/Order.cs
@@ -8,27 +8,43 @@ namespace E_CommerceSystemERD_Models.modles
 {
     public  class Order
     {
-        [key]
+        [Key]
         [Required]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int orderId { get; set; }
+        public int orderId { get; set; }//system genrated 
+
+
+
         [Required]
-        [ForeignKey("")]
-        public int  userId { get; set; }
+        [ForeignKey("user")]
+        public int  userId { get; set; }//
+        public User user { get; set; } // navigation property(user :order)
+
+
+
         [Required]
-        public DateTime orderDate { get; set; }
+        public DateTime orderDate { get; set; }//system gentrated
+
+
         [Required]
         [Range(0,double.MaxValue)]
-        public decimal totalAmount { get; set; } // must be greater than or equal to 0
+        public decimal totalAmount { get; set; } // system calculated 
+
+
         [Required]
         [MaxLength(30)]
-        public string   status { get; set; }= "Pending" 
+        public string status { get; set; } = "Pending";// default value
+
+
         [Required]
         [MaxLength(300)]
-        public string  shippingAddress { get; set; }
+        public string  shippingAddress { get; set; }//user input 
+
+
+
         [Required]
         [MaxLength(50)]
-        public string  paymentMethod { get; set; }
+        public string  paymentMethod { get; set; }//  from list — "CreditCard" | "DebitCard" | "PayPal" | "Cash"
 
 
 
