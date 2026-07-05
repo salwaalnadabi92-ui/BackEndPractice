7a405734588c75853d5e2694f82979a3a5fe55d2
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-07-05 15:22:00 +0400

    implement new class (oderitem)

--- E-CommerceSystemERD&Models/modles/orderItem.cs
+++ E-CommerceSystemERD&Models/modles/orderItem.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace E_CommerceSystemERD_Models.modles
@@ -7,6 +9,24 @@ namespace E_CommerceSystemERD_Models.modles
     public  class orderItem
 
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int orderItemId { get; set; }  // system generated
+
+
+        [Required]
+        [ForeignKey("order")]
+        public int orderId { get; set; } // foreign key //from list       
+         public Order order {  get; set; }// navigation property 
+
+
+        [Required]
+        [ForeignKey("Product")]
+        public int productId { get; set; } // foreign key //from list
+        public Product product { get; set; }// navigation property 
+
+
+
 
 
 
