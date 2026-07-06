9215cc8ea46bfffcdfcbdc4dd14518e5b16c5fa4
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-07-06 14:43:28 +0400

    implement ecommerce context

---
+++ E-CommerceSystemERD&Models/ECommerceContext.cs
@@ -0,0 +1,30 @@
+﻿using E_CommerceSystemERD_Models.modles;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace E_CommerceSystemERD_Models
+{
+    public class    ECommerceContext : DbContext
+
+    {
+
+        public DbSet<User> users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Category>  categories{  get; set; }
+        public DbSet<orderItem> OrderItems { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=localhost;Database=ECommerceDB;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+
+
+
+    }
+}
